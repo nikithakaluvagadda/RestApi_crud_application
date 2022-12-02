2415ccecd9291adc0746c4a6e8f214cfd6e1fbbe
Uday Teja k <uteja70@gmail.com>
2022-12-02 20:54:06 +0530

    adding test cases

--- test/src/student/routesTest.js
+++ test/src/student/routesTest.js
@@ -14,9 +14,64 @@ describe('CRUD Api', () => {
         it('It should GET all the student details', (done) => {
             chai.request(server).get('/api/v1/students')
                 .end((err, response)=> {
-                    expect('hello');
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
                 })
             done();
-        })
-    })
+        });
+
+        it("It should NOT GET all the student details", (done) => {
+            chai.request(server)
+                .get('/api/v1/students')
+                .end((err, response) => {
+                    response.should.have.status(404);
+                });
+                done();
+        });
+
+
+    });
+
+    describe('GET /api/v1/:id ', () => {
+        it('It should GET student details by id', (done) => {
+            const id = 10;
+            chai.request(server).get('/api/v1/students/' + id)
+                .end((err, response)=> {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                })
+            done();
+        });
+
+        it("It should NOT GET the student details by Id", (done) => {
+            const id = 10;
+            chai.request(server)
+                .get('/api/v1/students/' + id)
+                .end((err, response) => {
+                    response.should.have.status(404);
+                });
+                done();
+        });
+
+
+    });
+
+    describe('POST  /api/v1/addStudent', () => {
+        it('It should create the student details', (done) => {
+            const student  = {
+                "id" : 12,
+                "name" : "uday",
+                "email" : "asfd"
+            };
+            chai.request(server).post('/api/v1/students/addStudent')
+                .send(student)
+                .end((err, response)=> {
+                    response.should.have.status(201);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('name').eq("uday");
+                })
+            done();
+        });
+
+    });
 })
